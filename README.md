ca8fab12bed1553285f08b324c433ed96d34b99a
Ison <azzgo@users.noreply.github.com>
2022-07-27 22:41:23 +0800

    feat: remove feature and add es6 syntax

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Features
 * No deployment: JS only
 * Five visual states for services
 * Plays the [inception](http://inception.davepedu.com/) sound if a service fails
-* Polls server every 20 seconds
+* Polls server every 30 seconds
 * Non-blocking
 * Good for displaying on a flatscreen TV
 
@@ -31,44 +31,6 @@ Limitations
 
 Due to security reasons browsers do not allow cross-domain AJAX requests, therefore you have to configure your browser accordingly. Please do so at your own risk!
 
-Safari
-------
-
-Seems to work by default.
-
-Chrome
-------
-
-You will have to disable web security. Pass `-disable-web-security` when starting chrome.
-Eg. in OS X:
-
-    open /Applications/Google\ Chrome.app --args -disable-web-security
-
-Other Browsers
---------------
-
-Haven't tested yet. Please report in Issues if you can get it running on other browsers.
-
-Monitored Services
-==================
-
-* Heroku Apps
-* Heroku Tools
-* Amazon EC2 N.Virginia (Heroku lives there)
-* MongoHQ Rose
-* Mongolab DB
-* Pusher App
-* Rackspace Email
-* Github
-* Cloudmailin
-* Exceptional
-* London Underground Jubilee line (!)
-* Twilio
-* Plex
-* SickBeard
-* Couch Potato
-* SABnzbd
-
 Monitoring other services
 =========================
 
@@ -83,53 +45,8 @@ It is very easy to add any service you want. Just add another service to the `se
 
                                               // a function to parse the response.
                                               // must return true, false or "pending"
-      status: function(response) {            // In case of HTML response jQuery can be used
+      status: function(response) {            // 
         return JSON.parse(response)["status"] == "ok";
       }
     });
 
-Monitoring Jenkins CI
-----------------------
-
-Example of how to monitor Jenkins CI. Will display grey icon while building
-
-    Service({
-      name: 'Jenkins',
-      url: 'http://myjenkinsurl.com/job/Prject/lastBuild/api/json',
-      username: 'jenkinsuser',
-      password: 'someweirdpassword',
-      dataType: "json",
-      status: function(response) {
-        if(response["building"]){
-          return "pending";
-        };
-        return response["result"].toLowerCase() == "success";
-      }
-    });
-
-You shouldn't need to touch `app.js` at all.
-
-HTTPS URLs using self-signed certificates
------------------------------------------
-
-You may want to monitor an HTTPS service that uses a self-signed certificate. Your
-browser will abort request unless you trust that certificate. To do so, open the 
-service's URL in your browser, and accept whatever security exception your browser
-requires.
-
-Styling
-=======
-
-You should modify `src/screen.sass`. To compile it to css just run `sass src/screen.sass stylesheets/screen.css` in a project directory. You can also modify `stylesheets/screen.css` directly if you can't use sass for some serious reason.
-
-Contributing
-============
-
-Please make pull requests with useful services to monitor, SASS/CSS styles, new fancy sounds or any other cool features.
-
-Credits
-=======
-
-John Griffin, Tor Erik Linnerud, Tadas Tamosauskas
-
-Icons: [Glyphish free icon set](http://glyphish.com/)
--- app.js
+++ app.js
@@ -1,91 +1,134 @@
-SERVICES = []
+SERVICES = [];
+
+/**
+ * @type {Service}
+ * @property {string} name
+ * @property {string} url
+ * @property {(status) => boolean | 'pending'} statu
+ *
+ * @param {Service} service
+ * */
 function Service(service) {
-  SERVICES.push(service)
+  SERVICES.push(service);
 }
 
-$(function() {
-  var Statuses = {"true": "success", "false": "failure", "pending": "pending"};
+window.onload = function () {
+  var Statuses = { true: "success", false: "failure", pending: "pending" };
 
   function removeClasses(service) {
-    service.box.removeClass('pending unknown failure success waiting');
+    service.box.classList.remove("pending");
+    service.box.classList.remove("unknown");
+    service.box.classList.remove("failure");
+    service.box.classList.remove("success");
+    service.box.classList.remove("waiting");
   }
 
-  function handle_error(service, result, status) {
+  function handle_error(service) {
     removeClasses(service);
-    service.box.addClass('unknown');
-  }
-
-  function handle_result(service, result, status) {
-    if(status == 'success') {
-      if(result && result.replace)
-        result = result.replace(/body|img/gi, 'div')
-      status = service.status(result)
-      removeClasses(service)
-      service.box.addClass(Statuses[status]);
-      playIfNeeded(service, status);
-    } else {
-      handle_error(service, page, status)
-    }
+    service.box.classList.add("unknown");
   }
 
   function repeat(service) {
-    check = function() {
-      check_service(service)
-    }
-    setTimeout(check, service.interval || 20000);
+    check = function () {
+      check_service(service);
+    };
+    setTimeout(check, service.interval || 30000);
   }
 
   function check_service(service) {
-    service.box.addClass('waiting')
+    service.box.classList.add("waiting");
 
-    on_result = function(page, status) {  handle_result(service, page, status); repeat(service) }
-    on_error = function(page, status) {  handle_error(service, page, status); repeat(service) }
+    fetch(service.url, {
+      method: "GET",
+      timeout: 3500,
+    }).then(
+      (res) => {
+        if (res.status > 299 && res.status < 200) {
+          handle_error(service);
+          return;
+        }
 
-    $.ajax({
-       type: "GET",
-       url: service.url,
-       username: service.username,
-       password: service.password,
-       error: on_error,
-       success: on_result,
-       dataType: service.dataType,
-       jsonp: service.jsonp,
-       timeout: 3500,
-     });
+        res
+          .text()
+          .then((text) => {
+            const res = parseJSON(text);
+            const status = service.status(res);
+            removeClasses(service);
+            service.box.classList.add(Statuses[status]);
+            playIfNeeded(service, status);
+
+            repeat(service);
+          })
+          .catch((error) => {
+            console.error(error);
+            handle_error(service);
+          });
+      },
+      (error) => {
+        console.error(error);
+        handle_error(service);
+        repeat(service);
+      }
+    );
   }
 
-  function initialize_service(n, service) {
-    box = service.box = $('.service.template').clone()
-    box.removeClass('template')
-    box.find('.title').text(service.name)
-    $('#content').append(box)
+  function initialize_service(service) {
+    var box = document.querySelector(".service.template").cloneNode(true);
+    box.classList.remove("template");
+    box.querySelector(".title").innerText = service.name;
+
+    document.getElementById("content").appendChild(box);
+    service.box = box;
     check_service(service);
   }
 
-  function playIfNeeded(service, status){
+  function playIfNeeded(service, status) {
     if (service.lastStatus === undefined) {
       service.lastStatus = status;
       return null;
-    };
-    if (status === false && service.lastStatus !== false){
+    }
+    if (status === false && service.lastStatus !== false) {
       playNew();
-    };
+    }
     service.lastStatus = status;
-  };
+  }
 
-  $.each(SERVICES, initialize_service)
-})
+  SERVICES.forEach(initialize_service);
+};
 
 var id = 0;
-var playNew = function(){
+
+var playNew = function () {
   zone = document.getElementById("audioelements");
-  tag = document.createElement('audio');
-  tag.setAttribute("preload","auto");
-  tag.setAttribute("autoplay","autoplay");
-  tag.setAttribute("src","inception.mp3");
-  tag.setAttribute("id","audio"+id);
+  tag = document.createElement("audio");
+  tag.setAttribute("preload", "auto");
+  tag.setAttribute("autoplay", "autoplay");
+  tag.setAttribute("src", "inception.mp3");
+  tag.setAttribute("id", "audio" + id);
   zone.appendChild(tag);
   tag.play();
-  setTimeout("x=document.getElementById('audio"+id+"');x.parentNode.removeChild(x);", 8500);
+  setTimeout(
+    "x=document.getElementById('audio" + id + "');x.parentNode.removeChild(x);",
+    8500
+  );
   id++;
 };
+
+var parseJSON = function (jsonStr) {
+  try {
+    return JSON.stringify(jsonStr);
+  } catch (e) {
+    return jsonStr;
+  }
+};
+
+Service({
+  name: "Service",
+  url: "/",
+  status: function () {
+    const status = [true, false, "pending"][Math.floor(Math.random() * 3)];
+    console.log(status);
+    throw new Error("h");
+    return status;
+  },
+});
